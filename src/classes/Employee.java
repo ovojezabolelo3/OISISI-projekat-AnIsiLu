8f4978db7ed33bff7af4a8774b91264335efb708
Isidora <isa3693@gmail.com>
2022-05-18 18:26:38 +0200

    Popravka problema
    bin

--- src/classes/Employee.java
+++ src/classes/Employee.java
@@ -1,5 +1,7 @@
 package classes;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 //import classes.Adress;
 
 public class Employee {
@@ -16,5 +18,12 @@ int dateOfBirth;
 String email;
 
 //softver se isto poziva!!
+//koristiti for i if za proveru jmbg-a kod unosa korisnika 
 
+//for(i=0;i<=BrZaposlenih;i++) {
+//	if(jmbgUnet==jmbgPostojeci) 
+//		ispisi gresku 
+//	}
+
+//napravi neku petlju da proverava da li su uneti brojevi/slova u polja kad se pravi novi zaposleni 
 }
